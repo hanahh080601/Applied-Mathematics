7f7d3143c8faa1c0a36945801c13e99d49e8db58
hanahh080601 <hanlehngoc080601@gmail.com>
2021-09-19 23:50:22 +0700

    done det matrix

---
+++ detMatrix.py
@@ -0,0 +1,32 @@
+from copy import deepcopy
+m = int(input("Enter size of matrix: "))
+
+matrix = []
+for i in range(0,m):
+    print("Enter row", i + 1, ":")
+    temp = [int(input()) for j in range(0,m)]
+    matrix.append(temp) 
+print("Original matrix:")
+for i in range(m):
+    print(matrix[i])
+
+def smaller_matrix(a, row, col):
+    a_copy = deepcopy(a)
+    a_copy.remove(a[row])
+    for i in range(len(a_copy)):
+        a_copy[i].remove(a_copy[i][col])
+    return a_copy
+    
+def det(a):
+    num_rows = len(a)
+    if(num_rows == 2):
+        simple_det = a[0][0] * a[1][1] - a[0][1] * a[1][0]
+        return simple_det
+    else:
+        ans = 0
+        for j in range(len(a)):
+            cofactor = (-1) ** (0+j) * a[0][j] * det(smaller_matrix(a, 0, j))
+            ans += cofactor
+        return ans
+            
+print ("Det matrix: ", det(matrix))
